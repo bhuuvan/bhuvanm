d960dd88ea148752348968634408a2aaea3b57a9
bhuvanm <bhuone2799@gmail.com>
2023-09-10 00:12:56 -0500

    First C code

---
+++ Documents/Cpp/24_primitive_data_types.cpp
@@ -0,0 +1,28 @@
+/*
+Data types and variables in C++
+
+primitve:
+1. Integral -> int, char
+2. Bool -> bool
+3. Floating point -> float, double
+
+Data types:
+1. userdefined: enum, structure, union, class
+2. Derived: array, pointer, reference
+
+Data tpyes | Size  | Range'
+int | 2 or 4 | -32768 to 32767
+float | 4 |  -3.4e-38 to 3.4e38
+double | 8 | -1.7e-308 to 1.7e308
+char | 1 | -128 TO 127
+bool | UNDEFINED | true/false
+
+Modifiers:
+unsigned: unsigned int (0-65535), unsigned char: 0 - 255   {unsigned can be used only with char and int}
+
+
+long: long int, long double
+
+
+
+*/
